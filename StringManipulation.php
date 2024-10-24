bd16a825080fde72d3ff2018ec1dcb64022687ed
roanoke <roanoke@Roanoke.roanke.com>
2024-10-24 19:15:08 +0300

    Basic string manipulation

---
+++ StringManipulation.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <?php
+    $phrase = "To Be Or Not To Be";
+    //changing string to lower
+    echo strtolower ($phrase);
+    //change string to upper
+    echo strtoupper ($phrase);
+    //Number of strings
+    echo strlen ($phrase);
+    //firt character of the sting
+    echo $phrase [0];
+    ?>
+</body>
+</html>
