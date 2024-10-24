0f725bad5b1bb57f06eef2aa3a33a7b598ec3c07
roanoke <roanoke@Roanoke.roanke.com>
2024-10-24 17:35:29 +0300

    initializing and using variables

---
+++ Variables.php
@@ -0,0 +1,23 @@
+//VARIABLES
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $FirstName = "John";
+    $Age = 25 ;
+    echo "There once was a man named $FirstName <br/>";
+    echo "He was $Age years old <br/>";
+
+    $FirstName ="Mark";
+    $Age = 30;
+
+    echo "The name had Split personality disorder and believed his name to be $Firstname <br/>";
+    echo "and he belived his age to be $Age ";
+    ?>
+</body>
+</html>
